d9d7c6b261f32e0a1e193b51cb2d00178efb145d
perseu912 <perseu912@gmail.com>
2021-04-04 18:10:23 -0300

    bot para uatizap

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ venom.create().then((client) => start(client));
 function start(client){
     client.onMessage((message) => {
         if(message.body === 'oi'){
+
+        	console.log('mensagem ');
+        	console.log(message);
+        	console.log('cliente ');
+        	console.log(client);
             client.sendText(message.from,'oi seu corno, kkk')
         }
     })
